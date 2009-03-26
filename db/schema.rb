dae35a93934a7e76141d7a6f84097a269915c1e8
Paolo Montrasio <paolo@paolomontrasio.com>
2009-03-26 23:21:03 +0100

    Added db configuration for PostgreSQL Run the migrations to create the users and the arduinos tables.

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090318184820) do
+
+  create_table "arduinos", :force => true do |t|
+    t.string   "ip_address", :null => false
+    t.string   "drb"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+    t.string   "activation_code",           :limit => 40
+    t.datetime "activated_at"
+    t.string   "state",                                    :default => "passive"
+    t.datetime "deleted_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
