fbbdb4d2ca6795224fbcf0d74aa0fcd9dc39ca30
Paolo Montrasio <paolo@paolomontrasio.com>
2009-03-26 22:44:34 +0100

    Added missing files for restful_authentication to work. Restored the 750 mode on scripts

--- config/environment.rb
+++ config/environment.rb
@@ -55,4 +55,6 @@ Rails::Initializer.run do |config|
   config.gem "mislav-will_paginate", :version => "~> 2.3.6",
     :lib => "will_paginate", :source => "http://gems.github.com"
 
+  config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'
+
 end
