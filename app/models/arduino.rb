1d410635a792f076d66415662ad4efbb4342f617
Paolo Montrasio <paolo@paolomontrasio.com>
2009-03-29 18:06:37 +0200

    Added layout/stylesheet
    Added unique device key generation.

--- app/models/arduino.rb
+++ app/models/arduino.rb
@@ -1,7 +1,7 @@
 class Arduino < ActiveRecord::Base
 
   def Arduino.generate_device_key
-    "THISISNOTARANDOMKEY" # just for testing
+    Time.now.to_f.to_s.gsub(".", "") # Timestamp with milliseconds
   end
 
 end
