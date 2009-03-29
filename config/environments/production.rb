e49bcd0aca4a74d896dde7c347d2eebe0824b7f7
Paolo Montrasio <paolo@paolomontrasio.com>
2009-03-29 16:15:21 +0200

    Added the configuration for the smtp server and a proper home page for the application.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,3 +20,17 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_charset ="utf-8"
+config.action_mailer.perform_deliveries = false  # DO NOT SEND MAIL, THIS IS JUST A TEST
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.smtp_settings = {
+  :address => "something.something.net",
+  :port => 25,
+  :domain => "something.com",
+  :user_name => "something@something.com",
+  :password => "something",
+  :authentication => :login
+}
